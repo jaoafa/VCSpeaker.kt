2a71ffc90ec90aee58d06a19d6bb195e2904852a
yuua <identity@yuua.dev>
2024-02-27 23:13:18 +0900

    fix: user voice emotion

--- src/main/kotlin/com/jaoafa/vcspeaker/commands/VoiceCommand.kt
+++ src/main/kotlin/com/jaoafa/vcspeaker/commands/VoiceCommand.kt
@@ -29,10 +29,10 @@ class VoiceCommand : Extension() {
             name = "emotion"
             description = "感情"
 
-            choice("普通", "normal")
-
             for (value in Emotion.entries)
                 choice(value.emotionName, value.name)
+
+            choice("なし", "none")
         }
 
         val emotionLevel by optionalInt {
@@ -75,7 +75,7 @@ class VoiceCommand : Extension() {
                     val oldVoice = VoiceStore.byIdOrDefault(event.interaction.user.id)
 
                     val emotion = arguments.emotion?.let {
-                        if (it == "normal") null else Emotion.valueOf(it)
+                        if (it == "none") null else Emotion.valueOf(it)
                     }
 
                     val newVoice = oldVoice.overwrite(
