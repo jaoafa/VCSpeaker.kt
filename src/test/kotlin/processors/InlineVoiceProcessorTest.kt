5a7ae7b1fa78bb01fd5dbacc546caa3784d39704
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2024-06-18 22:11:35 +0900

    feat: add inline voice, markdown format test

--- src/test/kotlin/processors/InlineVoiceProcessorTest.kt
+++ src/test/kotlin/processors/InlineVoiceProcessorTest.kt
@@ -1,7 +1,110 @@
-package processors
-
+import com.jaoafa.vcspeaker.tts.Voice
+import com.jaoafa.vcspeaker.tts.api.Emotion
+import com.jaoafa.vcspeaker.tts.api.Speaker
+import com.jaoafa.vcspeaker.tts.processors.InlineVoiceProcessor
+import dev.kord.core.entity.Message
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.mockk
 
 class InlineVoiceProcessorTest : FunSpec({
-    // TODO: Write tests
+    test("Process returns original content when no inline voice parameters") {
+        val message = mockk<Message>()
+        val processor = InlineVoiceProcessor()
+        val voice = Voice(speaker = Speaker.Hikari)
+
+        val (processedText, processedVoice) = processor.process(message, "original content", voice)
+
+        processedText shouldBe "original content"
+        processedVoice shouldBe voice
+    }
+
+    test("Process returns content with inline voice parameters") {
+        val message = mockk<Message>()
+        val processor = InlineVoiceProcessor()
+        val voice = Voice(speaker = Speaker.Hikari)
+
+        val (processedText, processedVoice) = processor.process(
+            message,
+            "test speaker:haruka emotion:happiness emotion_level:1 pitch:100 speed:100",
+            voice
+        )
+
+        processedText shouldBe "test"
+        processedVoice shouldBe voice.copy(
+            speaker = Speaker.Haruka,
+            emotion = Emotion.Happiness,
+            emotionLevel = 1,
+            pitch = 100,
+            speed = 100
+        )
+    }
+
+    // 異常値
+    test("Process returns content with inline voice parameters with invalid speaker name") {
+        val message = mockk<Message>()
+        val processor = InlineVoiceProcessor()
+        val voice = Voice(speaker = Speaker.Hikari)
+
+        val exception = shouldThrow<IllegalArgumentException> {
+            processor.process(message, "test speaker:invalid", voice)
+        }
+        exception.message shouldBe "No enum constant com.jaoafa.vcspeaker.tts.api.Speaker.Invalid"
+    }
+
+    test("Process returns content with inline voice parameters with invalid emotion name") {
+        val message = mockk<Message>()
+        val processor = InlineVoiceProcessor()
+        val voice = Voice(speaker = Speaker.Hikari)
+
+        val exception = shouldThrow<IllegalArgumentException> {
+            processor.process(message, "test emotion:invalid", voice)
+        }
+        exception.message shouldBe "No enum constant com.jaoafa.vcspeaker.tts.api.Emotion.Invalid"
+    }
+
+    test("Process returns content with inline voice parameters with invalid emotion level") {
+        val message = mockk<Message>()
+        val processor = InlineVoiceProcessor()
+        val voice = Voice(speaker = Speaker.Hikari)
+
+        val (processedText, processedVoice) = processor.process(message, "test emotion_level:invalid", voice)
+
+        processedText shouldBe "test"
+        processedVoice shouldBe voice // emotionLevel は変更されない。ユーザーが入力ミスをした場合は無視される形になるので、良いか悪いか…
+    }
+
+    test("Process returns content with inline voice parameters with invalid pitch") {
+        val message = mockk<Message>()
+        val processor = InlineVoiceProcessor()
+        val voice = Voice(speaker = Speaker.Hikari)
+
+        val (processedText, processedVoice) = processor.process(message, "test pitch:invalid", voice)
+
+        processedText shouldBe "test"
+        processedVoice shouldBe voice // pitch は変更されない。ユーザーが入力ミスをした場合は無視される形になるので、良いか悪いか…
+    }
+
+    test("Process returns content with inline voice parameters with invalid speed") {
+        val message = mockk<Message>()
+        val processor = InlineVoiceProcessor()
+        val voice = Voice(speaker = Speaker.Hikari)
+
+        val (processedText, processedVoice) = processor.process(message, "test speed:invalid", voice)
+
+        processedText shouldBe "test"
+        processedVoice shouldBe voice // speed は変更されない。ユーザーが入力ミスをした場合は無視される形になるので、良いか悪いか…
+    }
+
+    test("Process returns content with inline voice parameters with invalid syntax") {
+        val message = mockk<Message>()
+        val processor = InlineVoiceProcessor()
+        val voice = Voice(speaker = Speaker.Hikari)
+
+        val (processedText, processedVoice) = processor.process(message, "test invalid:syntax", voice)
+
+        processedText shouldBe "test invalid:syntax"
+        processedVoice shouldBe voice
+    }
 })
--- src/test/kotlin/processors/MarkdownFormatProcessorTest.kt
+++ src/test/kotlin/processors/MarkdownFormatProcessorTest.kt
@@ -1,7 +1,79 @@
-package processors
-
+import com.jaoafa.vcspeaker.tts.Voice
+import com.jaoafa.vcspeaker.tts.api.Speaker
+import com.jaoafa.vcspeaker.tts.processors.MarkdownFormatProcessor
+import dev.kord.core.entity.Message
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.mockk
 
 class MarkdownFormatProcessorTest : FunSpec({
-    // TODO: Write tests
+    test("Process returns content converted from inline markdown") {
+        val message = mockk<Message>()
+        val processor = MarkdownFormatProcessor()
+        val voice = Voice(speaker = Speaker.Hikari)
+
+        val (processedText, processedVoice) = processor.process(
+            message,
+            "**bold** *italic* [link](http://example.com)",
+            voice
+        )
+
+        processedText shouldBe "bold italic link"
+        processedVoice shouldBe voice
+    }
+
+    test("Process returns content converted from block markdown") {
+        val message = mockk<Message>()
+        val processor = MarkdownFormatProcessor()
+        val voice = Voice(speaker = Speaker.Hikari)
+
+        val (processedText, processedVoice) = processor.process(
+            message, """
+            # Header 1
+            ## Header 2
+            ### Header 3
+            #### Header 4
+            ##### Header 5
+            ###### Header 6
+            - List item 1
+            - List item 2
+            - List item 3
+            1. Numbered list item 1
+            2. Numbered list item 2
+            3. Numbered list item 3
+            > Blockquote
+            `Code`
+            ```kotlin
+            fun main() {
+                println("Hello, world!")
+            }
+            ```
+        """.trimIndent(), voice
+        )
+
+        processedText shouldBe "# Header 1## Header 2### Header 3#### Header 4##### Header 5###### Header 6- List item 1- List item 2- List item 31. Numbered list item 12. Numbered list item 23. Numbered list item 3> BlockquoteCode"
+        processedVoice shouldBe voice
+    }
+
+    test("Process returns content with no changes when no markdown") {
+        val message = mockk<Message>()
+        val processor = MarkdownFormatProcessor()
+        val voice = Voice(speaker = Speaker.Hikari)
+
+        val (processedText, processedVoice) = processor.process(message, "no markdown here", voice)
+
+        processedText shouldBe "no markdown here"
+        processedVoice shouldBe voice
+    }
+
+    test("Process returns content with no changes when empty string") {
+        val message = mockk<Message>()
+        val processor = MarkdownFormatProcessor()
+        val voice = Voice(speaker = Speaker.Hikari)
+
+        val (processedText, processedVoice) = processor.process(message, "", voice)
+
+        processedText shouldBe ""
+        processedVoice shouldBe voice
+    }
 })
