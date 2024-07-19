8348d6e069cacbda6cb0160f99dd91740ff730a7
yuua <identity@yuua.dev>
2024-07-20 00:29:47 +0900

    feat: server mute notification

---
+++ src/main/kotlin/com/jaoafa/vcspeaker/events/ServerMuteEvent.kt
@@ -0,0 +1,62 @@
+package com.jaoafa.vcspeaker.events
+
+import com.jaoafa.vcspeaker.tts.narrators.Narrator.Companion.announce
+import com.kotlindiscord.kord.extensions.extensions.Extension
+import com.kotlindiscord.kord.extensions.extensions.event
+import com.kotlindiscord.kord.extensions.utils.isNullOrBot
+import dev.kord.common.entity.AuditLogEvent
+import dev.kord.core.behavior.getAuditLogEntries
+import dev.kord.core.event.user.VoiceStateUpdateEvent
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.first
+
+class ServerMuteEvent : Extension() {
+    override val name = this::class.simpleName!!
+    private val logger = KotlinLogging.logger { }
+
+    override suspend fun setup() {
+        event<VoiceStateUpdateEvent> {
+            check {
+                failIf(event.state.getMemberOrNull().isNullOrBot())
+                failIf(event.state.isMuted == event.old?.isMuted)
+            }
+
+            action {
+                val muted = event.state.isMuted
+                val hadMuted = event.old?.isMuted ?: return@action
+
+                val member = event.state.getMemberOrNull() ?: return@action
+                val guild = event.state.getGuildOrNull() ?: return@action
+
+                val auditLogEntry = guild.getAuditLogEntries {
+                    action = AuditLogEvent.MemberUpdate
+                }.filter {
+                    it.targetId == member.id
+                }.first()
+
+                val muter = auditLogEntry.userId?.let { guild.getMember(it) } ?: return@action
+
+                val memberName = member.effectiveName
+                val userMention = "@" + member.username
+                val muterMention = "@" + muter.username
+
+                if (muted && !hadMuted) {
+                    guild.announce(
+                        voice = "$memberName がサーバーミュートされました。",
+                        text = ":face_with_symbols_over_mouth: `$userMention` が `$muterMention` にサーバーミュートされました。"
+                    )
+
+                    logger.info { "[${guild.name}] Member muted: $userMention was muted by $muterMention" }
+                } else if (!muted && hadMuted) {
+                    guild.announce(
+                        voice = "$memberName のサーバーミュートが解除されました。",
+                        text = ":grinning: `$userMention` のサーバーミュートを `$muterMention` が解除しました。"
+                    )
+
+                    logger.info { "[${guild.name}] Member unmuted: $userMention was unmuted by $muterMention" }
+                }
+            }
+        }
+    }
+}
