9d7dec9dd2e48c814e412435558968e2c40dd33c
Tomachi <8929706+book000@users.noreply.github.com>
2024-12-21 23:55:40 +0900

    Revert "chore(deps): upgrade Cloud Vision to 3.53.0 (#260)"
    This reverts commit c0c4de038cdc95548d9d93e0c5a3fd6602f1f995.

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation("io.sentry:sentry:7.18.0")
     implementation("org.jsoup:jsoup:1.18.2")
     implementation("org.reflections:reflections:0.10.2")
-    implementation("com.google.cloud:google-cloud-vision:3.53.0")
+    implementation("com.google.cloud:google-cloud-vision:3.47.0")
     implementation("com.sksamuel.scrimage:scrimage-core:4.3.0")
     implementation("com.github.ajalt.clikt:clikt:5.0.1")
     implementation("com.uchuhimo:konf:1.1.2")
