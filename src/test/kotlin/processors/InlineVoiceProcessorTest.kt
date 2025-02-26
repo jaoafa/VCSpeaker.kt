c54018f244c483d7a5ab45f310d8f4d2a4fd881c
yuua <identity@yuua.dev>
2025-02-26 23:10:04 +0900

    fix: tests calling Message.getContent()

--- src/test/kotlin/processors/InlineVoiceProcessorTest.kt
+++ src/test/kotlin/processors/InlineVoiceProcessorTest.kt
@@ -9,6 +9,7 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.clearAllMocks
+import io.mockk.every
 import io.mockk.mockk
 
 /**
@@ -22,27 +23,29 @@ class InlineVoiceProcessorTest : FunSpec({
 
     // インライン音声パラメータがない場合、プロセスは元のコンテンツを返す
     test("If there is no inline voice parameters, the unchanged text should be returned.") {
+        val content = "original content"
+
         val message = mockk<Message>()
+        every { message.content } returns content
         val processor = InlineVoiceProcessor()
         val voice = Voice(speaker = Speaker.Hikari)
 
-        val (processedText, processedVoice) = processor.process(message, "original content", voice)
+        val (processedText, processedVoice) = processor.process(message, content, voice)
 
-        processedText shouldBe "original content"
+        processedText shouldBe content
         processedVoice shouldBe voice
     }
 
     // インライン音声パラメータがある場合、プロセスはコンテンツと音声パラメータを返す
     test("If inline voice parameters exist, the text and voice parameters should be returned.") {
+        val content = "test speaker:haruka emotion:happiness emotion_level:1 pitch:100 speed:100"
+
         val message = mockk<Message>()
+        every { message.content } returns content
         val processor = InlineVoiceProcessor()
         val voice = Voice(speaker = Speaker.Hikari)
 
-        val (processedText, processedVoice) = processor.process(
-            message,
-            "test speaker:haruka emotion:happiness emotion_level:1 pitch:100 speed:100",
-            voice
-        )
+        val (processedText, processedVoice) = processor.process(message, content, voice)
 
         processedText shouldBe "test"
         processedVoice shouldBe voice.copy(
@@ -56,35 +59,44 @@ class InlineVoiceProcessorTest : FunSpec({
 
     // 無効なスピーカー名が指定された場合、例外がスローされる
     test("If an invalid speaker name is specified, an exception should be thrown.") {
+        val content = "test speaker:invalid"
+
         val message = mockk<Message>()
+        every { message.content } returns content
         val processor = InlineVoiceProcessor()
         val voice = Voice(speaker = Speaker.Hikari)
 
         val exception = shouldThrow<IllegalArgumentException> {
-            processor.process(message, "test speaker:invalid", voice)
+            processor.process(message, content, voice)
         }
         exception.message shouldBe "No enum constant com.jaoafa.vcspeaker.tts.providers.voicetext.Speaker.Invalid"
     }
 
     // 無効な感情名が指定された場合、例外がスローされる
     test("If an invalid emotion name is specified, an exception should be thrown.") {
+        val content = "test emotion:invalid"
+
         val message = mockk<Message>()
+        every { message.content } returns content
         val processor = InlineVoiceProcessor()
         val voice = Voice(speaker = Speaker.Hikari)
 
         val exception = shouldThrow<IllegalArgumentException> {
-            processor.process(message, "test emotion:invalid", voice)
+            processor.process(message, content, voice)
         }
         exception.message shouldBe "No enum constant com.jaoafa.vcspeaker.tts.providers.voicetext.Emotion.Invalid"
     }
 
     // 無効な感情レベルが指定された場合、例外がスローされず、値が無視される
     test("If an invalid emotion level is specified, the parameter should be ignored without throwing an exception.") {
+        val content = "test emotion_level:invalid"
+
         val message = mockk<Message>()
+        every { message.content } returns content
         val processor = InlineVoiceProcessor()
         val voice = Voice(speaker = Speaker.Hikari)
 
-        val (processedText, processedVoice) = processor.process(message, "test emotion_level:invalid", voice)
+        val (processedText, processedVoice) = processor.process(message, content, voice)
 
         processedText shouldBe "test"
         processedVoice shouldBe voice // emotionLevel は変更されない。ユーザーが入力ミスをした場合は無視される形になるので、良いか悪いか…
@@ -92,11 +104,14 @@ class InlineVoiceProcessorTest : FunSpec({
 
     // 無効なピッチが指定された場合、例外がスローされず、値が無視される
     test("If an invalid pitch is specified, the parameter should be ignored without throwing an exception.") {
+        val content = "test pitch:invalid"
+
         val message = mockk<Message>()
+        every { message.content } returns content
         val processor = InlineVoiceProcessor()
         val voice = Voice(speaker = Speaker.Hikari)
 
-        val (processedText, processedVoice) = processor.process(message, "test pitch:invalid", voice)
+        val (processedText, processedVoice) = processor.process(message, content, voice)
 
         processedText shouldBe "test"
         processedVoice shouldBe voice // pitch は変更されない。ユーザーが入力ミスをした場合は無視される形になるので、良いか悪いか…
@@ -104,11 +119,14 @@ class InlineVoiceProcessorTest : FunSpec({
 
     // 無効なスピードが指定された場合、例外がスローされず、値が無視される
     test("If an invalid speed is specified, the parameter should be ignored without throwing an exception.") {
+        val content = "test speed:invalid"
+
         val message = mockk<Message>()
+        every { message.content } returns content
         val processor = InlineVoiceProcessor()
         val voice = Voice(speaker = Speaker.Hikari)
 
-        val (processedText, processedVoice) = processor.process(message, "test speed:invalid", voice)
+        val (processedText, processedVoice) = processor.process(message, content, voice)
 
         processedText shouldBe "test"
         processedVoice shouldBe voice // speed は変更されない。ユーザーが入力ミスをした場合は無視される形になるので、良いか悪いか…
@@ -116,11 +134,14 @@ class InlineVoiceProcessorTest : FunSpec({
 
     // インライン音声パラメータの構文が無効な場合、コンテンツと音声パラメータを返す
     test("If the syntax of the inline voice parameters is invalid, the parameters should be ignored.") {
+        val content = "test invalid:syntax"
+
         val message = mockk<Message>()
+        every { message.content } returns content
         val processor = InlineVoiceProcessor()
         val voice = Voice(speaker = Speaker.Hikari)
 
-        val (processedText, processedVoice) = processor.process(message, "test invalid:syntax", voice)
+        val (processedText, processedVoice) = processor.process(message, content, voice)
 
         processedText shouldBe "test invalid:syntax"
         processedVoice shouldBe voice
