655c1a947301d61b1542383d42b807d93d7d3a53
Tomachi <8929706+book000@users.noreply.github.com>
2026-01-02 03:31:23 +0900

    fix: StackOverflowエラーの原因となるapplyFiltersを削除 (#401)
    * fix: StackOverflowエラーの原因となるapplyFiltersを削除
    
    * fix: コンフリクト修正

--- src/main/kotlin/com/jaoafa/vcspeaker/tts/Scheduler.kt
+++ src/main/kotlin/com/jaoafa/vcspeaker/tts/Scheduler.kt
@@ -170,11 +170,7 @@ class Scheduler(
 
         // Speech 内に次の Track が存在し、かつ再生が可能な場合、次の Track を再生
         if (endReason.mayStartNext && next != null) {
-            val (nextTrack, nextContext) = next
-
-            link.player.applyFilters {
-                volume = if (nextContext is SoundmojiContext) 20F else 100F
-            }
+            val (nextTrack, _) = next
 
             link.player.playTrack(nextTrack)
             return
