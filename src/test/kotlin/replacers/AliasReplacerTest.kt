74cdca342a4b7c6dff3c56623064c1b77625ac0d
yuua <identity@yuua.dev>
2026-03-28 20:47:11 +0900

    feat: AliasReplacerTest migration

--- src/test/kotlin/replacers/AliasReplacerTest.kt
+++ src/test/kotlin/replacers/AliasReplacerTest.kt
@@ -1,62 +1,63 @@
 package replacers
 
 import com.jaoafa.vcspeaker.VCSpeaker
-import com.jaoafa.vcspeaker.stores.*
+import com.jaoafa.vcspeaker.database.DatabaseUtil
+import com.jaoafa.vcspeaker.database.actions.GuildAction.getEntity
+import com.jaoafa.vcspeaker.database.tables.AliasEntity
+import com.jaoafa.vcspeaker.database.tables.GuildEntity
+import com.jaoafa.vcspeaker.database.tables.GuildTable
+import com.jaoafa.vcspeaker.database.tables.VoiceEntity
+import com.jaoafa.vcspeaker.stores.AliasType
 import com.jaoafa.vcspeaker.tts.TextToken
 import com.jaoafa.vcspeaker.tts.replacers.AliasReplacer
 import dev.kord.common.entity.Snowflake
-import dev.kord.core.entity.Message
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import io.mockk.*
-import java.io.File
+import io.mockk.clearAllMocks
+import io.mockk.mockkObject
+import org.jetbrains.exposed.v1.jdbc.deleteAll
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import utils.createGuildMockk
+import utils.createMessageMockk
 
 class AliasReplacerTest : FunSpec({
+    beforeSpec {
+        DatabaseUtil.init("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1")
+        DatabaseUtil.createTables()
+    }
+
     // テスト前にモックを初期化
-    beforeTest {
+    beforeEach {
         mockkObject(VCSpeaker)
-        every { VCSpeaker.storeFolder } returns File(System.getProperty("java.io.tmpdir") + File.separator + "vcspeaker")
-
-        val storeStruct = mockk<StoreStruct<IgnoreData>>()
-        every { storeStruct.write() } returns Unit
-
-        mockkObject(IgnoreStore)
-        every { IgnoreStore.write() } returns Unit
-
-        IgnoreStore.data.clear()
-
-        mockkObject(AliasStore)
-        every { AliasStore.write() } returns Unit
-
-        AliasStore.data.clear()
+        transaction {
+            GuildEntity.new(id = Snowflake(0)) {
+                this.speakerVoiceEntity = VoiceEntity.new { }
+            }
+        }
     }
 
     // テスト後にモックを削除
-    afterTest {
+    afterEach {
+        transaction {
+            GuildTable.deleteAll()
+        }
         clearAllMocks()
-    }
-
-    // 全てのテスト後にフォルダを削除
-    afterSpec {
-        File(System.getProperty("java.io.tmpdir"), "vcspeaker").deleteRecursively()
     }
 
     // テキストエイリアスを設定した場合、正しく置き換えられる
     test("If a text alias matches the message, the replaced text should be returned.") {
-        val message = mockk<Message>()
-        coEvery { message.getGuild() } returns mockk {
-            every { id } returns Snowflake(0)
-        }
+        val guild = createGuildMockk(Snowflake(0))
+        val message = createMessageMockk(Snowflake(0))
 
-        AliasStore.create(
-            AliasData(
-                guildId = Snowflake(0),
-                userId = Snowflake(0),
-                type = AliasType.Text,
-                search = "world",
+        transaction {
+            AliasEntity.new {
+                guildEntity = guild.getEntity()
+                creatorDid = Snowflake(0)
+                type = AliasType.Text
+                search = "world"
                 replace = "Kotlin"
-            )
-        )
+            }
+        }
 
         val tokens = mutableListOf(TextToken("Hello, world!"))
         val expectedTokens = mutableListOf(TextToken("Hello, "), TextToken("Kotlin", "Text Alias「world」→「Kotlin」"), TextToken("!"))
@@ -68,20 +69,18 @@ class AliasReplacerTest : FunSpec({
 
     // テキストエイリアスを設定していても合致しない場合、変更されない
     test("If a text alias does not match the content, the text should remain unchanged.") {
-        val message = mockk<Message>()
-        coEvery { message.getGuild() } returns mockk {
-            every { id } returns Snowflake(0)
-        }
+        val guild = createGuildMockk(Snowflake(0))
+        val message = createMessageMockk(Snowflake(0))
 
-        AliasStore.create(
-            AliasData(
-                guildId = Snowflake(0),
-                userId = Snowflake(0),
-                type = AliasType.Text,
-                search = "Java",
+        transaction {
+            AliasEntity.new {
+                guildEntity = guild.getEntity()
+                creatorDid = Snowflake(0)
+                type = AliasType.Text
+                search = "Java"
                 replace = "Kotlin"
-            )
-        )
+            }
+        }
 
         val tokens = mutableListOf(TextToken("Hello, world!"))
 
