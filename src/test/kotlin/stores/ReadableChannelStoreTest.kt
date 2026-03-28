2d3df42d11ebc6d627818fb4b64252053ed26395
yuua <identity@yuua.dev>
2026-03-29 00:42:36 +0900

    feat: ReadableChannelStoreTest migration

--- src/test/kotlin/stores/ReadableChannelStoreTest.kt
+++ src/test/kotlin/stores/ReadableChannelStoreTest.kt
@@ -1,64 +1,95 @@
 package stores
 
-import com.jaoafa.vcspeaker.VCSpeaker
-import com.jaoafa.vcspeaker.stores.ReadableChannelData
-import com.jaoafa.vcspeaker.stores.ReadableChannelStore
+import com.jaoafa.vcspeaker.database.DatabaseUtil
+import com.jaoafa.vcspeaker.database.actions.GuildAction.getEntity
+import com.jaoafa.vcspeaker.database.actions.ReadableChannelAction.isReadableChannel
+import com.jaoafa.vcspeaker.database.onDuplicate
+import com.jaoafa.vcspeaker.database.tables.GuildEntity
+import com.jaoafa.vcspeaker.database.tables.GuildTable
+import com.jaoafa.vcspeaker.database.tables.VoiceEntity
+import com.jaoafa.vcspeaker.database.transactionResulting
+import com.jaoafa.vcspeaker.database.unwrap
 import dev.kord.common.entity.Snowflake
-import dev.kord.core.entity.channel.TextChannel
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.mockkObject
-import java.io.File
+import io.mockk.clearAllMocks
+import org.jetbrains.exposed.v1.core.and
+import org.jetbrains.exposed.v1.core.eq
+import org.jetbrains.exposed.v1.exceptions.ExposedSQLException
+import org.jetbrains.exposed.v1.jdbc.deleteAll
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import utils.createGuildMockk
+import utils.createTextChannelMockk
+import com.jaoafa.vcspeaker.database.tables.ReadableChannelEntity as Entity
+import com.jaoafa.vcspeaker.database.tables.ReadableChannelTable as Table
 
 class ReadableChannelStoreTest : FunSpec({
     val guildId1 = Snowflake(111111111111111111UL)
     val guildId2 = Snowflake(222222222222222222UL)
     val channelId1 = Snowflake(333333333333333333UL)
     val channelId2 = Snowflake(444444444444444444UL)
-    val addedByUserId = Snowflake(0UL)
-
-    /**
-     * 指定した ID を持つ TextChannel のモックを生成します。
-     */
-    fun mockChannel(channelId: Snowflake) = mockk<TextChannel> {
-        every { id } returns channelId
-    }
+    val creatorId = Snowflake(0UL)
 
     beforeSpec {
-        mockkObject(VCSpeaker)
-        every { VCSpeaker.storeFolder } returns File(System.getProperty("java.io.tmpdir") + File.separator + "vcspeaker")
-        VCSpeaker.storeFolder.mkdirs()
-        // ReadableChannelStore 初期化用の空ファイルを作成（未作成の場合のみ）
-        val readableChannelFile = File(VCSpeaker.storeFolder, "readablechannels.json")
-        if (!readableChannelFile.exists()) {
-            readableChannelFile.writeText("[]")
+        DatabaseUtil.init("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1")
+        DatabaseUtil.createTables()
+    }
+
+    beforeEach {
+        transaction {
+            GuildEntity.new(id = guildId1) {
+                this.speakerVoiceEntity = VoiceEntity.new { }
+            }
+            GuildEntity.new(id = guildId2) {
+                this.speakerVoiceEntity = VoiceEntity.new { }
+            }
         }
     }
 
-    afterTest {
-        ReadableChannelStore.data.clear()
+    afterEach {
+        transaction {
+            GuildTable.deleteAll()
+        }
+        clearAllMocks()
     }
 
     // --- isReadableChannel ---
 
     context("isReadableChannel") {
         test("If the channel is registered, should return true.") {
-            ReadableChannelStore.data.add(ReadableChannelData(guildId1, channelId1, addedByUserId))
+            val guild = createGuildMockk(guildId1)
+            val channel = createTextChannelMockk(channelId1, guildId1)
 
-            ReadableChannelStore.isReadableChannel(guildId1, mockChannel(channelId1)) shouldBe true
+            transaction {
+                Entity.new {
+                    guildEntity = guild.getEntity()
+                    channelDid = channel.id
+                    creatorDid = creatorId
+                }
+            }
+
+            channel.isReadableChannel() shouldBe true
         }
 
         test("If the channel is not registered, should return false.") {
-            ReadableChannelStore.isReadableChannel(guildId1, mockChannel(channelId1)) shouldBe false
+            val channel = createTextChannelMockk(channelId1, guildId1)
+            channel.isReadableChannel() shouldBe false
         }
 
         test("If the guild ID differs even if the channel ID matches, should return false.") {
-            ReadableChannelStore.data.add(ReadableChannelData(guildId1, channelId1, addedByUserId))
+            val guild = createGuildMockk(guildId1)
+            val channel = createTextChannelMockk(channelId1, guildId2)
 
-            ReadableChannelStore.isReadableChannel(guildId2, mockChannel(channelId1)) shouldBe false
+            transaction {
+                Entity.new {
+                    guildEntity = guild.getEntity()
+                    channelDid = channelId1
+                    creatorDid = creatorId
+                }
+            }
+
+            channel.isReadableChannel() shouldBe false
         }
     }
 
@@ -66,24 +97,75 @@ class ReadableChannelStoreTest : FunSpec({
 
     context("add") {
         test("If a channel is added, the data count should increase.") {
-            ReadableChannelStore.add(guildId1, mockChannel(channelId1), addedByUserId)
+            val guild = createGuildMockk(guildId1)
+            val channel = createTextChannelMockk(channelId1, guildId1)
 
-            ReadableChannelStore.data shouldHaveSize 1
-            ReadableChannelStore.data.first().channelId shouldBe channelId1
+            transaction {
+                Entity.new {
+                    guildEntity = guild.getEntity()
+                    channelDid = channel.id
+                    creatorDid = creatorId
+                }
+
+                Entity.all().count() shouldBe 1
+                Entity.find { Table.channelDid eq channel.id }.count() shouldBe 1
+            }
         }
 
         test("If the same channel is added twice, the data count should not increase.") {
-            ReadableChannelStore.add(guildId1, mockChannel(channelId1), addedByUserId)
-            ReadableChannelStore.add(guildId1, mockChannel(channelId1), addedByUserId)
+            val guild = createGuildMockk(guildId1)
+            val channel = createTextChannelMockk(channelId1, guildId1)
 
-            ReadableChannelStore.data shouldHaveSize 1
+            transaction {
+                Entity.new {
+                    guildEntity = guild.getEntity()
+                    channelDid = channel.id
+                    creatorDid = creatorId
+                }
+            }
+
+            var duplicate = false
+
+            shouldThrow<ExposedSQLException> {
+                transactionResulting {
+                    Entity.new {
+                        guildEntity = guild.getEntity()
+                        channelDid = channel.id
+                        creatorDid = creatorId
+                    }
+                }.onDuplicate {
+                    duplicate = true
+                }.unwrap()
+            }
+
+            duplicate shouldBe true
+
+            transaction {
+                Entity.all().count() shouldBe 1
+            }
         }
 
         test("If the same channel ID is added for different guilds, they should be stored as separate entries.") {
-            ReadableChannelStore.add(guildId1, mockChannel(channelId1), addedByUserId)
-            ReadableChannelStore.add(guildId2, mockChannel(channelId1), addedByUserId)
+            val guild1 = createGuildMockk(guildId1)
+            val guild2 = createGuildMockk(guildId2)
+            val channel = createTextChannelMockk(channelId1, guildId1)
 
-            ReadableChannelStore.data shouldHaveSize 2
+            transaction {
+                Entity.new {
+                    guildEntity = guild1.getEntity()
+                    channelDid = channel.id
+                    creatorDid = creatorId
+                }
+                Entity.new {
+                    guildEntity = guild2.getEntity()
+                    channelDid = channel.id
+                    creatorDid = creatorId
+                }
+            }
+
+            transaction {
+                Entity.all().count() shouldBe 2
+            }
         }
     }
 
@@ -91,54 +173,76 @@ class ReadableChannelStoreTest : FunSpec({
 
     context("remove") {
         test("If a channel is removed, the data for that channel should be deleted.") {
-            ReadableChannelStore.data.add(ReadableChannelData(guildId1, channelId1, addedByUserId))
+            val guild = createGuildMockk(guildId1)
+            val channel = createTextChannelMockk(channelId1, guildId1)
 
-            ReadableChannelStore.remove(guildId1, mockChannel(channelId1))
-
-            ReadableChannelStore.data shouldHaveSize 0
+            transaction {
+                Entity.new {
+                    guildEntity = guild.getEntity()
+                    channelDid = channel.id
+                    creatorDid = creatorId
+                }
+            }
+            transaction {
+                Entity.find { (Table.guildDid eq guild.id) and (Table.channelDid eq channel.id) }.single().delete()
+            }
+            transaction {
+                Entity.all().count() shouldBe 0
+            }
         }
 
         test("If a channel is removed, data for the same channel ID in a different guild should not be deleted.") {
-            ReadableChannelStore.data.add(ReadableChannelData(guildId1, channelId1, addedByUserId))
-            ReadableChannelStore.data.add(ReadableChannelData(guildId2, channelId1, addedByUserId))
+            val guild1 = createGuildMockk(guildId1)
+            val guild2 = createGuildMockk(guildId2)
+            val channel = createTextChannelMockk(channelId1, guildId1)
 
-            ReadableChannelStore.remove(guildId1, mockChannel(channelId1))
-
-            ReadableChannelStore.data shouldHaveSize 1
-            ReadableChannelStore.data.first().guildId shouldBe guildId2
+            transaction {
+                Entity.new {
+                    guildEntity = guild1.getEntity()
+                    channelDid = channel.id
+                    creatorDid = creatorId
+                }
+                Entity.new {
+                    guildEntity = guild2.getEntity()
+                    channelDid = channel.id
+                    creatorDid = creatorId
+                }
+            }
+            transaction {
+                Entity.find { (Table.guildDid eq guild1.id) and (Table.channelDid eq channel.id) }.single().delete()
+            }
+            transaction {
+                Entity.all().count() shouldBe 1
+                Entity.find { (Table.guildDid eq guild2.id) and (Table.channelDid eq channel.id) }.count() shouldBe 1
+            }
         }
 
         test("If a channel is removed, data for a different channel in the same guild should not be deleted.") {
-            ReadableChannelStore.data.add(ReadableChannelData(guildId1, channelId1, addedByUserId))
-            ReadableChannelStore.data.add(ReadableChannelData(guildId1, channelId2, addedByUserId))
+            val guild1 = createGuildMockk(guildId1)
+            val channel1 = createTextChannelMockk(channelId1, guildId1)
+            val channel2 = createTextChannelMockk(channelId2, guildId2)
 
-            ReadableChannelStore.remove(guildId1, mockChannel(channelId1))
+            transaction {
+                Entity.new {
+                    guildEntity = guild1.getEntity()
+                    channelDid = channel1.id
+                    creatorDid = creatorId
+                }
+                Entity.new {
+                    guildEntity = guild1.getEntity()
+                    channelDid = channel2.id
+                    creatorDid = creatorId
+                }
+            }
 
-            ReadableChannelStore.data shouldHaveSize 1
-            ReadableChannelStore.data.first().channelId shouldBe channelId2
-        }
-    }
+            transaction {
+                Entity.find { (Table.guildDid eq guild1.id) and (Table.channelDid eq channel1.id) }.single().delete()
+            }
 
-    // --- removeForGuild ---
-
-    context("removeForGuild") {
-        test("If removeForGuild is called, all data for the specified guild should be deleted.") {
-            ReadableChannelStore.data.add(ReadableChannelData(guildId1, channelId1, addedByUserId))
-            ReadableChannelStore.data.add(ReadableChannelData(guildId1, channelId2, addedByUserId))
-
-            ReadableChannelStore.removeForGuild(guildId1)
-
-            ReadableChannelStore.data shouldHaveSize 0
-        }
-
-        test("If removeForGuild is called, data for a different guild should not be deleted.") {
-            ReadableChannelStore.data.add(ReadableChannelData(guildId1, channelId1, addedByUserId))
-            ReadableChannelStore.data.add(ReadableChannelData(guildId2, channelId1, addedByUserId))
-
-            ReadableChannelStore.removeForGuild(guildId1)
-
-            ReadableChannelStore.data shouldHaveSize 1
-            ReadableChannelStore.data.first().guildId shouldBe guildId2
+            transaction {
+                Entity.all().count() shouldBe 1
+                Entity.find { (Table.guildDid eq guild1.id) and (Table.channelDid eq channel2.id) }.count() shouldBe 1
+            }
         }
     }
 })
--- src/test/kotlin/utils/CreateMockk.kt
+++ src/test/kotlin/utils/CreateMockk.kt
@@ -3,6 +3,7 @@ package utils
 import dev.kord.common.entity.Snowflake
 import dev.kord.core.entity.Guild
 import dev.kord.core.entity.Message
+import dev.kord.core.entity.channel.TextChannel
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
@@ -21,4 +22,9 @@ fun createMessageMockk(guildId: Snowflake) = mockk<Message>(relaxed = true).appl
 
 fun createGuildMockk(guildId: Snowflake) = mockk<Guild>().apply {
     every { id } returns guildId
+}
+
+fun createTextChannelMockk(channelId: Snowflake, guildId: Snowflake) = mockk<TextChannel>().apply {
+    every { this@apply.guildId } returns guildId
+    every { id } returns channelId
 }
