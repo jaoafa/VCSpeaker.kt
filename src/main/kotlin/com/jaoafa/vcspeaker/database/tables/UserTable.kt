3e4235c47a2c468f91e52ec05aa1efcf47c6aac5
yuua <identity@yuua.dev>
2026-08-25 01:17:22 +0900

    fix: implement SnappableEntity on UserEntity

--- src/main/kotlin/com/jaoafa/vcspeaker/database/tables/UserTable.kt
+++ src/main/kotlin/com/jaoafa/vcspeaker/database/tables/UserTable.kt
@@ -1,12 +1,17 @@
 package com.jaoafa.vcspeaker.database.tables
 
 import com.jaoafa.vcspeaker.database.DatabaseUtil.version
+import com.jaoafa.vcspeaker.database.EntitySnapshot
+import com.jaoafa.vcspeaker.database.SnappableEntity
+import com.jaoafa.vcspeaker.database.SnapshotFactory
 import com.jaoafa.vcspeaker.database.SnowflakeEntity
 import com.jaoafa.vcspeaker.database.SnowflakeEntityClass
 import com.jaoafa.vcspeaker.database.SnowflakeIdTable
 import com.jaoafa.vcspeaker.database.VersionedTable
 import dev.kord.common.entity.Snowflake
+import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.v1.core.dao.id.EntityID
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 
 object UserTable : SnowflakeIdTable("vcs_user", columnName = "did"), VersionedTable {
     val voiceId = reference(
@@ -16,8 +21,29 @@ object UserTable : SnowflakeIdTable("vcs_user", columnName = "did"), VersionedTa
     override val version = version()
 }
 
-class UserEntity(id: EntityID<Snowflake>) : SnowflakeEntity(id) {
+class UserEntity(id: EntityID<Snowflake>) : SnowflakeEntity(id), SnappableEntity<UserSnapshot, UserEntity> {
     companion object : SnowflakeEntityClass<UserEntity>(UserTable)
 
     var voiceEntity by VoiceEntity referencedOn UserTable.voiceId
+
+    override fun getSnapshot() = transaction { UserSnapshot.from(readValues) }
+}
+
+@Serializable
+data class UserSnapshot(
+    val did: Snowflake,
+    val voiceId: Int,
+    val version: Int,
+) : EntitySnapshot<UserEntity>() {
+    companion object : SnapshotFactory<UserSnapshot> {
+        override fun from(row: org.jetbrains.exposed.v1.core.ResultRow) = UserSnapshot(
+            did = row[UserTable.id].value,
+            voiceId = row[UserTable.voiceId].value,
+            version = row[UserTable.version],
+        )
+    }
+
+    override fun getEntity() = transaction {
+        UserEntity[this@UserSnapshot.did]
+    }
 }
