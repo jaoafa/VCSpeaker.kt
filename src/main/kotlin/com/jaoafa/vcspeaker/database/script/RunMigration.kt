5d1cf4153728db1c3c44264bd0386585a84c2732
yuua <identity@yuua.dev>
2026-09-22 22:03:27 +0900

    fix: fail with code 1 when migration ran from RunMigration.kt (and it failed)

--- src/main/kotlin/com/jaoafa/vcspeaker/database/script/RunMigration.kt
+++ src/main/kotlin/com/jaoafa/vcspeaker/database/script/RunMigration.kt
@@ -2,9 +2,18 @@ package com.jaoafa.vcspeaker.database.script
 
 import com.jaoafa.vcspeaker.database.DatabaseUtil
 import com.jaoafa.vcspeaker.database.DatabaseUtil.DEFAULT_DB_URL
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlin.system.exitProcess
+
+private val logger = KotlinLogging.logger { }
 
 fun main() {
     val databaseUrl = System.getenv("DATABASE_URL") ?: DEFAULT_DB_URL
 
-    DatabaseUtil.migrate(databaseUrl)
+    val isMigrationSuccessful = DatabaseUtil.migrate(databaseUrl)
+
+    if (!isMigrationSuccessful) {
+        logger.error { "Database migration failed. Exiting..." }
+        exitProcess(1)
+    }
 }
